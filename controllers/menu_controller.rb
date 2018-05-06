97fb172d1982b6916fb13443ca9f1f4beb9bb19d
mleonardo93 <mleonardo@gmail.com>
2018-05-06 19:52:27 -0400

    checkpoint 5 assignment

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
     def main_menu
         puts "Main Menu - #{address_book.entries.count} entries"
         puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - View entry number <n>"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         print "Enter your selection: "
 
         selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
                 main_menu
             when 2
                 system "clear"
-                create_entry
+                view_specific_entry
                 main_menu
             when 3
                 system "clear"
-                search_entries
+                create_entry
                 main_menu
             when 4
                 system "clear"
+                search_entries
+                main_menu
+            when 5
+                system "clear"
                 read_csv
                 main_menu
-            when 5 
+            when 6 
                 puts "Goodbye!"
                 exit(0)
             else
@@ -69,6 +74,20 @@ class MenuController
 
         system "clear"
         puts "New entry created"
+    end
+
+    def view_specific_entry
+        system "clear"
+        selection = (gets.chomp.to_i - 1)
+        if selection < address_book.entries.length
+            puts address_book.entries[selection]
+            puts "Press m to return to main menu"
+            gets.chomp
+            system "clear"
+        else 
+            puts "Entry number #{selection} does not exist in the address book"
+            view_specific_entry
+        end
     end
 
     def search_entries
